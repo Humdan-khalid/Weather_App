e5475ac962e56fe1a8ff74da11f8dd97ad2b007c
Hamdan Khalid <hamdankhalid663@gmail.com>
2026-06-11 21:14:17 +0500

    testing in weather

--- app/tests/Services/test_weather_service.py
+++ app/tests/Services/test_weather_service.py
@@ -22,24 +22,29 @@ async def test_user_not_found(mock_auth):
         )
 
 @patch(
-    "app.services.weather_service.get_weather_data_from_cache"
+    "app.services.weather_service.auth_repo.user_authentication_with_email"
 )
 
 @patch(
-    "app.services.weather_service.weather_repo.save_weather_history"
+    "app.services.weather_service.get_weather_data_from_cache"
 )
 
 @patch(
     "app.services.weather_service.api_call.get_weather_from_api"
 )
 
+@patch(
+    "app.services.weather_service.weather_repo.save_weather_history"
+)
+
 @pytest.mark.asyncio
 async def test_cache_hit(
-    mock_auth,
+    fake_history,
+    mock_api,
     mock_cache,
-    mock_save_history,
-    mock_live_weather_api
-):
+    mock_auth
+    ):
+
     fake_user = Mock()
 
     fake_user.id = 1
@@ -55,11 +60,67 @@ async def test_cache_hit(
 
     result = await get_live_weather(
         "Karachi",
-        AsyncMock,
+        AsyncMock(),
         {
             "email": "hamdan@gmail.com"
         }
     )
     
     assert result == fake_cache
+    mock_api.assert_not_awaited()
+
+@patch(
+        "app.services.weather_service.weather_repo.save_weather_history"
+)
+
+@patch(
+    "app.services.weather_service.caching.set_cached"
+)
+
+@patch(
+        "app.services.weather_service.api_call.get_weather_from_api"
+)
+
+@patch(
+        "app.services.weather_service.get_weather_data_from_cache"
+)
+
+@patch(
+        "app.services.weather_service.auth_repo.user_authentication_with_email"
+)
+
+
+@pytest.mark.asyncio
+async def test_weather_api_hit(
+        fake_auth,
+        fake_cache,
+        fake_weather_api,
+        fake_save_cache,
+        fake_data_save
+        ):
     
+    fake_user = Mock()
+
+    fake_user.id = 1
+
+    fake_auth.return_value = fake_user
+
+    fake_cache.return_value = None
+
+    weather = {
+        "temperature": "33",
+        "weather": "Cloudy"
+    }
+
+    fake_weather_api.return_value = weather
+    fake_cache.return_value = weather
+
+    result = await get_live_weather(
+        "Lahore",
+        AsyncMock(),
+        {
+            "email": "hamdan@gmail.com"
+        } 
+    )
+
+    assert result == weather
